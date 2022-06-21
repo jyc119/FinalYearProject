b4832b3ed0830cea607d18a0f10d834c9822bf7f
jyc119 <60002815+jyc119@users.noreply.github.com>
2022-06-21 17:53:19 +0100

    Added Diode Simulation

---
+++ DiodeSimulation/DiodeHelper.fs
@@ -0,0 +1,47 @@
+﻿module DiodeHelper
+
+open Extreme.Mathematics
+open Extreme.Mathematics.LinearAlgebra
+
+type DiodeData = {
+    Conductance : ((int * int) * float) list
+    ConductanceDerivative : (int * int) list
+    Current : float list
+    CurrentDiodeElement : int list
+    Vd : (int * int)
+}
+
+let getNodeNumber (lst : (int*int) list) =
+    let maxX = List.maxBy fst lst |> fst
+    let maxY = List.maxBy snd lst |> snd
+
+    if maxX > maxY 
+    then maxX
+    else maxY
+
+let firstPart (lst : ((int * int)*float) list) = 
+    lst
+    |> List.map(fun x -> fst x)
+
+let getMatrix (lst : ((int*int) * float) list ) = 
+
+    let coordList = firstPart lst
+    let nodeNumber = getNodeNumber coordList 
+    let mat = Matrix.Create(nodeNumber,nodeNumber)
+
+    (mat , lst)
+    ||> List.fold (fun mat lst -> let index = fst lst
+                                  let value = snd lst
+                                  let firstindex = fst index
+                                  let secondindex = snd index
+                                  mat.[(firstindex-1),(secondindex-1)] <- value
+                                  mat.[(secondindex-1),(firstindex-1)] <- value
+                                  mat
+                          )
+
+let addOnVector (vec:DenseVector<float>)(lst : int list)(diodeC:float) = 
+    (vec, lst)
+    ||> List.fold(fun vecRes index -> let original = vecRes[index-1]
+                                      let sum = original + diodeC
+                                      vecRes[index-1] <- -sum
+                                      vecRes)
---
+++ DiodeSimulation/Program.fs
@@ -0,0 +1,75 @@
+﻿module DiodeSimulation
+
+open System
+open System.IO
+open Extreme.Mathematics
+open Extreme.Mathematics.LinearAlgebra
+open DiodeHelper
+
+
+let saturationCurrent = 1e-15
+let vt = 0.025875
+
+/// Creates the jacobian matrix
+let createJacobian (circuitMat: DenseMatrix<float>) (conductDerivative: (int*int) list)(derivative: float) = 
+    (circuitMat, conductDerivative)
+    ||> List.fold(fun mat index -> let first = fst index
+                                   let second = snd index
+                                   let original = mat.[(first-1) , (second-1)]
+                                   let sum = original + derivative
+                                   mat.[(first-1) , (second-1)] <- sum
+                                   mat)
+
+/// Creates the voltage solution at an iteration
+let createVoltageSol (nodenumber:int)= 
+    let listmat = [1..nodenumber]
+    let vec:DenseVector<float> = Vector.Create(nodenumber)
+    (vec,listmat)
+    ||> List.fold (fun mat el -> mat[(el-1)] <- 1.0
+                                 mat)
+
+/// Calculates the function vector 
+let createFunctionVec (elements: float [])(conductanceList:((int*int)*float) list)(diodeCurrent: float)(voltage: DenseVector<float>)(currentdiodeel: int list) = 
+    let sumVector = Vector.Create(elements)
+    let sumVectorAdd = addOnVector sumVector currentdiodeel diodeCurrent
+    let originalConductance = getMatrix conductanceList
+    (originalConductance * voltage) - sumVectorAdd
+
+
+/// Diode simulation
+let diodeSimulation (data: DiodeData) = 
+    
+    let nodeNumber = getNodeNumber ( firstPart data.Conductance)
+    
+    // Iniital Voltage Solution (V1..Vn)
+    let mutable (voltageSolution:DenseVector<float>) = 
+        createVoltageSol nodeNumber
+    
+    let mutable voltageDiff = 10.0
+    let conductanceList = data.Conductance
+
+    while voltageDiff>0.0005 do 
+        
+        let (circuitMatrix:DenseMatrix<float>) = getMatrix conductanceList
+        let vd = 
+            match data.Vd with
+            | (0,2) -> voltageSolution[1]
+            | (0,1) -> voltageSolution[0]
+            | (1,2) -> voltageSolution[0] - voltageSolution[1]
+            | (2,1) -> voltageSolution[1] - voltageSolution[0] 
+        //printfn "Vd: %A" (vd)
+        let diodeCurrent = saturationCurrent * (exp(vd/vt) - 1.0)
+        let derivative = (saturationCurrent/vt) * exp(vd/vt)
+        let Jacobian = 
+            createJacobian circuitMatrix data.ConductanceDerivative derivative
+        
+        let elements = data.Current
+                       |> List.toArray
+        let functionVector = createFunctionVec elements conductanceList diodeCurrent voltageSolution data.CurrentDiodeElement
+    
+
+        let jac_inv = Jacobian.GetInverse()
+        let deltaVector = jac_inv * (-1.*functionVector)
+        let nextVoltage = deltaVector + voltageSolution
+        voltageDiff <- abs(voltageSolution[0] - nextVoltage[0])
+        voltageSolution <- nextVoltage.ToDenseVector()
